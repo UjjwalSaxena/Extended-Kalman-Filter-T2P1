186745ec9e071ed30bc34c8c1b3fba8c0bb51c1b
UjjwalSaxena <er.ujjwalsaxena@gmail.com>
2018-03-23 13:30:56 +0530

    Update kalman_filter.cpp

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -23,27 +23,13 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
-  /**
-  TODO:
-    * predict the state
-  */
-
   x_= F_* x_;
-//   std::cout<<"====after predict====="<<std::endl;
-//   std::cout<<P_<<std::endl;
-//   std::cout<< x_<<std::endl;
-//   std::cout<<x_<<std::endl;
   P_ = F_* P_ * F_.transpose() + Q_;
-//   std::cout<<"========="<<std::endl;
-//   std::cout<<P_<<std::endl;
-//   std::cout<< x_<<std::endl;
+
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Kalman Filter equations
-  */
+
   VectorXd predicted_= H_* x_;
   MatrixXd y_= z- predicted_;
 //   MatrixXd HT_= H_.transpose();
@@ -69,33 +55,24 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   float rho_= sqrt(px_*px_+ py_*py_);
   float theta= atan2(py_,px_);
   float rho_dot;
-  if(fabs(rho_)>0.0001)
-    rho_dot = (px_*vx_+ py_*vy_)/rho_;
-  else
+  if(fabs(rho_)<0.0001)
   {
-
     px_ += .001;
     py_ += .001;
-    rho_dot = sqrt(px_ * px_ + py_ * py_);
+    rho_= sqrt(px_*px_+ py_*py_);
   }
-    
+  rho_dot = (px_*vx_+ py_*vy_)/rho_;  
   z_predicted_polar<< rho_,theta,rho_dot;
   
   VectorXd y_= z- z_predicted_polar;
   y_[1]= tools.NormalizePhi(y_[1]);
-//   std::cout<<"====theta====="<<std::endl;
-//   std::cout<<y_[1]<<std::endl;
   MatrixXd HT_= H_.transpose(); 
   MatrixXd K_= P_ * HT_ * ( H_ * P_* HT_ + R_ ).inverse();// Kalman Gain
   x_= x_ + (K_ * y_);
-  
  
   MatrixXd I_ = MatrixXd::Identity(4, 4);
   P_= (I_- K_ * H_) * P_;
-  /**
-  TODO:
-    * update the state by using Extended Kalman Filter equations
-  */
+
 }
 
 
